64c914a93c61b6a9b1b882680d42da685dbabbc5
Iglek Kuat <dizinnes228@gmail.com>
2020-10-26 23:06:18 +0600

    feat: create Header.jsx

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+      <header className='header'>
+        <img className='header-logo' src="https://www.logaster.com/blog/wp-content/uploads/2018/05/LogoMakr.png" alt=""/>
+      </header>
+  );
+}
+
+export default Header;
