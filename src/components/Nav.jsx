9368ab9760bb90dfda3e731e52dcf92ba231822d
Iglek Kuat <dizinnes228@gmail.com>
2020-10-26 23:06:36 +0600

    feat: create Nav.jsx

---
+++ src/components/Nav.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+const Nav = () => {
+  return (
+    <nav className='nav'>
+      <div>
+        <a href="Profile">Profile</a>
+      </div>
+
+      <div>
+        <a href="#Messages">Messages</a>
+      </div>
+
+      <div>
+        <a href="#News">News</a>
+      </div>
+
+      <div>
+        <a href="#Music">Music</a>
+      </div>
+
+      <div>
+        <a href="#Settings">Settings</a>
+      </div>
+
+      <div>
+        <p>test</p>
+      </div>
+    </nav>
+  );
+}
+
+export default Nav;
