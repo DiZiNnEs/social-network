1e37fb4f96d6f50d8b7a574fcc583e5a175dc529
Iglek Kuat <dizinnes228@gmail.com>
2020-10-26 18:09:40 +0600

    feat: delete headers, main, footer components

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import React from "react";
 
 import './App.css';
 
-import Technologies from "./Technologies";
-import Header from "./Header";
-import Footer from "./Footer";
+// import Technologies from "./Technologies";
+// import Header from "./Header";
+// import Footer from "./Footer";
 
-const App =  () => {
+const App = () => {
   return (
     <div>
       <Header />
