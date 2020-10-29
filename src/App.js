d075d33fbfde902ea82e3dfdccb571faf2d46164
Iglek Kuat <dizinnes228@gmail.com>
2020-10-30 02:31:55 +0600

    feat: put the Header in the directory

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 
 import './App.css';
 
-import Header from "./components/Header";
+import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar";
 import Profile from "./components/Profile/Profle";
 
