c36f20ad712ea5c18a81c4e4903bd4531b25895c
Iglek Kuat <dizinnes228@gmail.com>
2020-10-30 03:16:02 +0600

    feat: update all schemes of posts

--- src/components/Profile/Post.jsx
+++ src/components/Profile/Post.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import classes from './styles/Profile.module.css'
 
 const Post = () => {
   return (
     <div>
-      post 1
+      <div className={classes.item}>
+        <img
+          src="https://e7.pngegg.com/pngimages/9/692/png-clipart-social-media-computer-icons-avatar-user-internet-social-media-computer-network-logo.png"
+          alt=""/>
+      </div>
+      <div>Best post</div>
     </div>
   );
 }
--- src/components/Profile/Posts.jsx
+++ src/components/Profile/Posts.jsx
@@ -4,6 +4,11 @@ const Posts = () => {
   return (
     <div>
       Posts
+      <div>
+        <textarea name="" id="" cols="20" rows="10"></textarea>
+        <button>Add post</button>
+        <button>Cancel</button>
+      </div>
     </div>
   )
 }
