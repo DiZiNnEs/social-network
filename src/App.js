c7573593a5b49bef816cef0ee8b8f5af44dcd6b2
Iglek Kuat <dizinnes228@gmail.com>
2020-11-01 04:12:05 +0600

    feat: add BrowserRouter with Route for routing messages and profile components

--- src/App.js
+++ src/App.js
@@ -6,15 +6,20 @@ import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
 import Profile from "./components/Profile/Profle";
 import Dialog from "./components/Dialogs/Diaolog";
+import {Route, BrowserRouter} from 'react-router-dom';
 
 const App = () => {
   return (
-    <div className='app-wrapper'>
-      <Header/>
-      <Navbar/>
-      {/*<Profile/>*/}
-      <Dialog/>
-    </div>
+    <BrowserRouter>
+      <div className='app-wrapper'>
+        <Header/>
+        <Navbar/>
+        <div className='content'>
+          <Route path='/messages' component={Dialog}/>
+          <Route path='/profile' component={Profile}/>
+        </div>
+      </div>
+    </BrowserRouter>
   );
 }
 
