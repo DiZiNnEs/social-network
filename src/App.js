5e4c2e3152295958556ec21177e8d040a833054a
Iglek Kuat <dizinnes228@gmail.com>
2020-11-01 03:29:45 +0600

    feat: create Dialog component and connect to App.js

--- src/App.js
+++ src/App.js
@@ -5,13 +5,15 @@ import './App.css';
 import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
 import Profile from "./components/Profile/Profle";
+import Dialog from "./components/Dialogs/Diaolog";
 
 const App = () => {
   return (
     <div className='app-wrapper'>
       <Header/>
       <Navbar/>
-      <Profile/>
+      {/*<Profile/>*/}
+      <Dialog/>
     </div>
   );
 }
---
+++ src/components/Dialogs/Diaolog.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import classes from './styles/Diaolog.module.css'
+
+const Dialog = () => {
+  return (
+    <div className={classes}>
+      <p>Dialog</p>
+    </div>
+  );
+}
+
+export default Dialog;
