689a30a53b5199e6bcd320657a14da2ada80474b
Iglek Kuat <dizinnes228@gmail.com>
2020-11-01 18:53:25 +0600

    feat: instead of `a` to `NavLink`

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,27 +1,29 @@
 import React from 'react';
 import classes from './styles/Navbar.module.css';
+import {NavLink} from 'react-router-dom';
+
 
 const Navbar = () => {
   return (
     <nav className={classes.nav}>
       <div className={classes.item}>
-        <a href="/profile">Profile</a>
+        <NavLink to='/profile'>Profile</NavLink>
       </div>
 
       <div className={classes.item}>
-        <a href="/messages">Messages</a>
+        <NavLink to='/messages'>Messages</NavLink>
       </div>
 
       <div className={classes.item}>
-        <a href="/news">News</a>
+        <NavLink to='/news'>News</NavLink>
       </div>
 
       <div className={classes.item}>
-        <a href="/music">Music</a>
+        <NavLink to='/music'>Music</NavLink>
       </div>
 
       <div className={classes.item}>
-        <a href="/settings">Settings</a>
+        <NavLink to='/settings'>Settings</NavLink>
       </div>
     </nav>
   );
