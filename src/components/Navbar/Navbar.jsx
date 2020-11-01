3187a8a0cbadb93be160155aeef30e8dd108f08d
Iglek Kuat <dizinnes228@gmail.com>
2020-11-01 19:38:41 +0600

    feat: add highlight the current link in golden colour

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,29 +1,29 @@
 import React from 'react';
 import classes from './styles/Navbar.module.css';
-import {NavLink} from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 
 const Navbar = () => {
   return (
     <nav className={classes.nav}>
       <div className={classes.item}>
-        <NavLink to='/profile'>Profile</NavLink>
+        <NavLink to='/profile' activeClassName={classes.active__link}>Profile</NavLink>
       </div>
 
       <div className={classes.item}>
-        <NavLink to='/messages'>Messages</NavLink>
+        <NavLink to='/messages' activeClassName={classes.active__link}>Messages</NavLink>
       </div>
 
       <div className={classes.item}>
-        <NavLink to='/news'>News</NavLink>
+        <NavLink to='/news' activeClassName={classes.active__link}>News</NavLink>
       </div>
 
       <div className={classes.item}>
-        <NavLink to='/music'>Music</NavLink>
+        <NavLink to='/music' activeClassName={classes.active__link}>Music</NavLink>
       </div>
 
       <div className={classes.item}>
-        <NavLink to='/settings'>Settings</NavLink>
+        <NavLink to='/settings' activeClassName={classes.active__link}>Settings</NavLink>
       </div>
     </nav>
   );
