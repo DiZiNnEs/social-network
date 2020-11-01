0f54ebfaebe45c83e43e311d1c78ea524aba7103
Iglek Kuat <dizinnes228@gmail.com>
2020-11-01 16:09:29 +0600

    feat: create Settings jsx file and connect style

---
+++ src/components/Settings/Settings.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import classes from './styles/Settings.module.css'
+
+const Settings = (props) => {
+  return (
+    <div>
+      <p className={classes.p__settings}>settings</p>
+    </div>
+  );
+}
+
+export default Settings;
