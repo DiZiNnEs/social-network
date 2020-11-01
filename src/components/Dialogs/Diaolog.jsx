5daf19a9da7982291e3409a9e40e5c7610740874
Iglek Kuat <dizinnes228@gmail.com>
2020-11-02 01:38:18 +0600

    feat: add full dialog and under dialogs system in layouts

--- src/components/Dialogs/Diaolog.jsx
+++ src/components/Dialogs/Diaolog.jsx
@@ -3,8 +3,32 @@ import classes from './styles/Diaolog.module.css'
 
 const Dialog = () => {
   return (
-    <div className='content'>
-      <p className={classes.p__dialog}>Dialog</p>
+    <div className={classes.dialogs}>
+      <div className={classes.dialogs_items}>
+        <div className={classes.items}>
+          Shapagat
+        </div>
+        <div className={classes.items}>
+          Alisher
+        </div>
+        <div className={classes.items + ' ' + classes.active}>
+          Dizi
+        </div>
+        <div className={classes.items}>
+          Toki
+        </div>
+        <div className={classes.items}>
+          Denis
+        </div>
+        <div className={classes.items}>
+          Evgenij
+        </div>
+      </div>
+      <div className={classes.messages}>
+        <div className={classes.messages_items}>Hello</div>
+        <div className={classes.messages_items}>Hi, how are you</div>
+        <div className={classes.messages_items}>You're welcome</div>
+      </div>
     </div>
   );
 }
