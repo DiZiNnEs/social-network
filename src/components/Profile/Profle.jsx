4715db95b781505e333f0f643a478a05032de02a
Iglek Kuat <dizinnes228@gmail.com>
2020-11-03 15:44:41 +0600

    refactor: divide on component and add Top.jsx

--- src/components/Profile/Profle.jsx
+++ src/components/Profile/Profle.jsx
@@ -1,25 +1,17 @@
 import React from 'react';
 import classes from './styles/Profile.module.css';
-import Post from "./Post";
 import Posts from "./Posts";
-import Banner from "./Banner";
+import Top from "./Top";
 
 const Profile = () => {
   return (
     <main className='content'>
-      <Banner image={'https://www.gettyimages.pt/gi-resources/images/Homepage/Hero/PT/PT_hero_42_153645159.jpg'}/>
 
       <div>
-        <img src="https://q4x7t3q8.map2.ssl.hwcdn.net/assets/images/members/avamax-m1.jpg" alt="error_avatar"/>
+        <Top/>
       </div>
-
-      <div>
-
-        <div className={classes.posts}>
-          <Posts/>
-        </div>
-
-
+      <div className={classes.posts}>
+        <Posts/>
       </div>
 
     </main>
