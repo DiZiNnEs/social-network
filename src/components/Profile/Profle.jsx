3823f0285d92093e65385561c4dbfef973dd1ba6
Iglek Kuat <dizinnes228@gmail.com>
2020-11-03 14:52:27 +0600

    feat: connect Banner to Profile.jsx

--- src/components/Profile/Profle.jsx
+++ src/components/Profile/Profle.jsx
@@ -2,14 +2,12 @@ import React from 'react';
 import classes from './styles/Profile.module.css';
 import Post from "./Post";
 import Posts from "./Posts";
+import Banner from "./Banner";
 
 const Profile = () => {
   return (
     <main className='content'>
-      <div>
-        <img src="https://www.gettyimages.pt/gi-resources/images/Homepage/Hero/PT/PT_hero_42_153645159.jpg"
-             alt="error_main_content_image"/>
-      </div>
+      <Banner image={'https://www.gettyimages.pt/gi-resources/images/Homepage/Hero/PT/PT_hero_42_153645159.jpg'}/>
 
       <div>
         <img src="https://q4x7t3q8.map2.ssl.hwcdn.net/assets/images/members/avamax-m1.jpg" alt="error_avatar"/>
