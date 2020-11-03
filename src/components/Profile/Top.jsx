77885c7525c3c45db88ab34b9b22143712298030
Iglek Kuat <dizinnes228@gmail.com>
2020-11-03 15:44:17 +0600

    feat: create Top.jsx

---
+++ src/components/Profile/Top.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Banner from "./Banner";
+
+const Top = (props) => {
+  return (
+    <div>
+      <div>
+        <Banner image={'https://www.gettyimages.pt/gi-resources/images/Homepage/Hero/PT/PT_hero_42_153645159.jpg'}/>
+      </div>
+      <div>
+        <img src="https://q4x7t3q8.map2.ssl.hwcdn.net/assets/images/members/avamax-m1.jpg" alt="error_avatar"/>
+      </div>
+
+    </div>
+  );
+}
+
+export default Top;
