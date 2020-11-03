9f20868f69dc92d351a88d474432cf9f1e1abd37
Iglek Kuat <dizinnes228@gmail.com>
2020-11-03 14:51:58 +0600

    feat: create Banner for Profile

---
+++ src/components/Profile/Banner.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Banner = (props) => {
+  return (
+    <div>
+      <img src={props.image}
+           alt="error_main_content_image"/>
+    </div>
+  );
+}
+
+export default Banner;
